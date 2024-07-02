0b480bf44cf02013b7bc731c9f44086eeaae7935
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-03 00:50:47 +0530

    Feat: Add server action to fetch data from api

---
+++ app/action.ts
@@ -0,0 +1,32 @@
+"use server";
+// Server action -> Use this directive at the top of the file to indicate that this code should be executed on the server side
+// Here having one function whose work is to fetch data from api 
+// Server-side execution ensures that sensitive information are not exposed to the client
+
+
+export const fetchAnime = async (page: number) => {
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}$limit=8&order=popularity`);
+
+    const data = await response.json();
+
+    // console.log(data);
+
+    return data;
+}
+
+
+
+// In contrast, client-side code would use the "use client" directive
+// "use client"; // Client action -> Use this directive at the top of the file to indicate that this code should be executed on the client side.
+// Client-side code is used for tasks like rendering the UI, handling user interactions, and manipulating the DOM.
+// Example of a client-side function 
+
+/*
+export const displayAnime = (animeData) => {
+    // Code to render anime data on the UI
+};
+
+
+Directives like "use server" and "use client" help in maintaining a clear separation of responsibilities between the server and the client, improving code organization and maintainability.
+
+*/
