147d89b7b5e59a318590460d09b5ad8cdabd00bd
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-03 20:41:13 +0530

    Update: Hero Section, img and fonts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
+import { TextGenerateEffect } from "./ui/TextGenerating";
 
 function Hero() {
+  const words = `Discover the Enchanting Worlds of Anime.`
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
-      <div className="flex-1 flex flex-col gap-10">
+      <div className="flex-1 flex flex-col">
         <Image
-          src="./logo.svg"
+          src="./animeTransparent.svg"
           alt="logo"
-          width={101}
-          height={96}
+          width={201}
+          height={196}
           className="object-contain"
         />
-        <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-        Discover the <span className="red-gradient">Enchanting Worlds</span> of
-          Anime.
-        </h1>
+        <TextGenerateEffect words={words} />
       </div>
       <div className="lg:flex-1 relative w-full h-[70vh] justify-center">
-        <Image src="/levy.png" alt="anime" fill className="object-contain" />
+        <Image src="/levy.svg" alt="anime" fill className="object-contain" />
       </div>
     </header>
   );
