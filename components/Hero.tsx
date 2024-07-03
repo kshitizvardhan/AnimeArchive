cd036b98f44cfbae2efd4a16485b416bae25dc97
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-03 21:29:32 +0530

    Bug fixes on smaller screen devices

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   const words = `Discover the Enchanting Worlds of Anime.`
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col max-md:items-center">
         <Image
           src="./animeTransparent.svg"
           alt="logo"
@@ -15,7 +15,7 @@ function Hero() {
         />
         <TextGenerateEffect words={words} />
       </div>
-      <div className="lg:flex-1 relative w-full h-[70vh] justify-center">
+      <div className="lg:flex-1 relative w-full h-[70vh] justify-center hidden md:block">
         <Image src="/levy.svg" alt="anime" fill className="object-contain" />
       </div>
     </header>
