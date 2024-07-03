e4d2df7771062af54894897e8c4cb70582065e91
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-03 20:35:51 +0530

    Add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={dmSans.className}>
         <main className="max-w-7xl mx-auto bg-[#0F1117]">
           <Hero />
